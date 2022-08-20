71a047a71bb0c4ae61275007ed19bacee94dd791
Dariam89 <daria.morawska89@gmail.com>
2022-08-20 09:18:10 +0200

    Add TrainerRepository

---
+++ src/main/java/inqooprojectbe/repositories/TrainerRepository.java
@@ -0,0 +1,10 @@
+package inqooprojectbe.repositories;
+
+import inqooprojectbe.model.Trainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainerRepository extends JpaRepository<Trainer,Long> {
+
+}
+
+
