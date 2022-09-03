8e687ea1c3060d7851d52516093253391ba9eed6
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-09-03 16:27:00 +0200

    Added Schedule controller, service and repository

---
+++ src/main/java/inqooprojectbe/model/ScheduleMapper.java
@@ -0,0 +1,13 @@
+package inqooprojectbe.model;
+
+public class ScheduleMapper {
+    public ScheduleDTO toDTO(Schedule schedule){
+        ScheduleDTO scheduleDTO = new ScheduleDTO(schedule.getWorkshopUUID(), schedule.getTrainerUUID(), schedule.getStartDate());
+        return scheduleDTO;
+    }
+
+    public Schedule fromDTO(ScheduleDTO scheduleDTO){
+        Schedule schedule = new Schedule( scheduleDTO.getWorkshopUUID(), scheduleDTO.getTrainerUUID(), scheduleDTO.getStartDate());
+        return schedule;
+    }
+}
